63faf8ad935eab100a1e63b5f5638f9d286914a2
melitacasola <melissacasola@gmail.com>
2023-12-15 17:31:21 +0100

    ok first mobile

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,29 @@ async function getRandomDataFromApi() {
   const response =  await fetch(api_url);
   const data =  await response.json()
 
-  randomDataElement.innerHTML = data.text
+  randomDataElement.innerHTML = `
+  <div class="main__container">
+  <h1>GET A RANDOM FACT</h1>
+  <div class="main__container-line">
+    <img src="./src/assets/icons/decoration.svg" alt="decoration main page">
+  </div>
+  <section class="main__container-section">
+
+    <div class="book__container">
+      <div>${data.text}</div>
+    </div>
+    
+  </section>
+  <div class="btn__container">
+    <button id="btn-fact" class="btn__primary"><img src="./src/assets/icons/next-icon.svg" alt="go to another fact">Get another fact</button>
+    <button type="button" class="btn__second" id="liveToastBtn"><img src="./src/assets/icons/favourite-icon.svg" alt="add to favourite">Add to favourites</button>
+  </div>
+  <div class="links">
+    <a href="#">Take me to my favourites</a>
+  </div>
+
+</div>
+  `
 }
 
 // const printData = () => {document.querySelector('#app').innerHTML = `
